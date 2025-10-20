7beae7ddfaa8644b3123cc4584b6ed64b10cd070
Flavio Grilli <flavio.grilli@ynov.com>
2025-10-20 12:08:12 +0200

    feat : menu.html

--- src/main.go
+++ src/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
 	"os"
 )
 
 func main() {
+
+	listTemplates, errTemplate := template.ParseGlob("./templates/*.html")
+	if errTemplate != nil {
+		fmt.Println(errTemplate.Error())
+		os.Exit(1)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		listTemplates.ExecuteTemplate(w, "menu", nil)
+	})
 
 	path, _ := os.Getwd()
 	fileServer := http.FileServer(http.Dir(path + "/assets"))

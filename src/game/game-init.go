4e50e1281e7d22cccb85edf363d3e4d1902b005a
Flavio Grilli <flavio.grilli@ynov.com>
2025-10-23 09:26:18 +0200

    feat : module game
    feat : vérifications

---
+++ src/game/game-init.go
@@ -0,0 +1,19 @@
+package game
+
+import (
+	"errors"
+	"regexp"
+	"strings"
+)
+
+func TraitementPlayerInit(name string) error {
+
+	name = strings.TrimSpace(name)
+	reg := regexp.MustCompile(`^[A-Za-z]{4,20}$`)
+
+	if !reg.MatchString(name) {
+		return errors.New("nom invalide : 4 à 20 lettres uniquement (ex : Bobby)")
+	}
+
+	return nil
+}
--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"game/game"
 	"html/template"
 	"net/http"
 	"os"
@@ -17,6 +18,23 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		listTemplates.ExecuteTemplate(w, "menu", nil)
+	})
+
+	http.HandleFunc("/game/init", func(w http.ResponseWriter, r *http.Request) {
+		listTemplates.ExecuteTemplate(w, "game-init", nil)
+	})
+
+	http.HandleFunc("/game/init/traitement", func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Method != http.MethodPost {
+			http.Redirect(w, r, "/game/init", http.StatusSeeOther)
+		}
+
+		name1 := r.FormValue("pseudoJoueur1")
+		name2 := r.FormValue("pseudoJoueur2")
+
+		game.TraitementPlayerInit(name1)
+		game.TraitementPlayerInit(name2)
 	})
 
 	path, _ := os.Getwd()
